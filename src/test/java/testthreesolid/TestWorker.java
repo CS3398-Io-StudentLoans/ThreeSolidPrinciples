925d04345ceff74b26ec58935d0265d9a50de8ec
Daniel Le <ledanieln@gmail.com>
2018-10-23 17:34:13 -0500

    testWorker

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,32 +1,17 @@
 package ThreeSolidPrinciples;
 
-import org.junit.jupiter.api.BeforeAll;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 public class TestWorker {
 
-   private  Worker w = new Worker();
-	
-   @Test
-   @DisplayName("No Name Test")
-   public void testWorkerWithNoName()
-   {
-      assertEquals(w.getName(),"");
-      assertEquals(w.work(),"I'm working already!");
-   }
+   private Worker w = new Worker();
 
    @Test
-   @DisplayName("With Name Test")
-   public void testWorkerWithName()
-   {
-      w.setName("Corvus Glaive");
-      assertEquals(w.getName(),"Corvus Glaive");
-      assertEquals(w.work(),"Corvus Glaive is working very hard!");
-   }
+   @DisplayName("Work Method Test")
+   
+public void testWorkMethod() {
 
+   assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
 }
