2ed29615c48c61794d26a4e3baa4748ff8952a64
Ted Lehr <t_l81@txstate.edu>
2018-10-26 08:08:30 -0500

    Resolved the merge conflict

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -25,20 +25,11 @@ public class TestWorker {
    		assertEquals(w.work(),"I am working..","Mismatch between test text and method text");
 
 	}
-<<<<<<< HEAD
-	
-	@Test
-	@DisplayName("I'm a Failure")
-	static void fail("I'm a big failure") {
-	}
 	
 	
-	
-=======
 	@Test
 	@DisplayName("Fail Method Test")
 	public void testFail(){
 		assertEquals(w.work(), "this is failing", "showing a fail test method");
 	}
->>>>>>> parent of 3c662fb... fixed Jays error :)
 }
